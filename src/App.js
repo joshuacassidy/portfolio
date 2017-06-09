15d08ee578138396b46cab034aa1dd4d94ad2c76
joshuacassidy <Joshcassidy79@gmail.com>
2017-06-09 21:33:40 +0100

    fetching data using json and jquery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Header from './Components/Header';
 import About from './Components/About';
 import Resume from './Components/Resume';
@@ -8,12 +7,41 @@ import Testimonials from './Components/Testimonials';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 import './App.css';
+import $ from 'jquery'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData:{}
+    }
+  }
+
+  getResumeData(){
+    $.ajax({
+      url: 'htt[://localhost:3000/resumeData.json',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData:data})
+      }.bind(this),
+      error: function(xhr,status,err){
+        console.log(err);
+        alert(err);
+      }
+    })
+  }
+
+  componetDidMount(){
+    this.getResumeData();
+  }
+
   render() {
+    console.log(this.state.resumeData)
     return (
       <div className="App">
-app
+
       <Header />
       <About />
       <Resume />
