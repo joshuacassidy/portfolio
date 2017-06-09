d90fa663f1fb1069013c0c5f5ffaf1cf6f9b31b8
joshuacassidy <Joshcassidy79@gmail.com>
2017-06-09 23:47:06 +0100

    finished resume section

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -30,9 +30,10 @@ class Resume extends Component {
       });
 
       var skills = this.props.data.skills.map(function(skill){
-        var className = 'bar-expand '+skill.name.toLowerCase();
-        return <li key={skill.name}><span style={{width:skill.level}} className={className}></span><em>{skill.name}</em></li>
+            var className = 'bar-expand '+skill.name.toLowerCase();
+            return <li key={skill.name}><span style={{width:skill.level}} className={className}></span><em>{skill.name}</em></li>
       });
+      var skills_description = this.props.data.skills_description;
     }
     return (
       <section id="resume">
@@ -61,11 +62,7 @@ class Resume extends Component {
             <h1><span>Skills</span></h1>
          </div>
          <div className="nine columns main-col">
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-            voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt.
-            </p>
+            <p>{skills_description}</p>
 				<div className="bars">
 				   <ul className="skills">
 					  {skills}
