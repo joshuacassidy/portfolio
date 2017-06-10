11b3cf00d7b26f2948c946a3da51a5b08c0d92e4
joshuacassidy <Joshcassidy79@gmail.com>
2017-06-10 17:03:18 +0100

    removed sample data from the description, education, work, and social media sections from the data file

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,6 +13,7 @@ class Header extends Component {
     }
     return (
       <header id="home">
+
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
@@ -30,7 +31,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">Im {name}.</h1>
-            <h3>Im a {city} based <span>{occupation}</span> {description}</h3>
+            <h3>I'm a {city} based <span>{occupation},</span> {description}</h3>
             <hr />
             <ul className="social">
               {networks}
