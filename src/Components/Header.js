a67d9785f6e1b91055f2847dcf398f4a1cd692df
joshuacassidy <Joshcassidy79@gmail.com>
2017-06-23 20:11:35 +0100

    fixed nav bar menu error

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,7 +20,7 @@ class Header extends Component {
 
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="/#" title="Hide navigation">Hide navigation</a>
+	      <a className="mobile-btn" href="#nav" title="Hide navigation">Hide navigation</a>
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
